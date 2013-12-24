fa6b50ffd792f231f17bda9c16d4e6fdb989330b
Adrian Lanning <alanning@gmail.com>
2013-12-23 21:23:06 -0500

    fix typo

--- revert-require-shim.js
+++ revert-require-shim.js
@@ -16,13 +16,13 @@
     this.require = this.__AC_OLD_REQUIRE;
     safeDelete(this, "__AC_OLD_REQUIRE");
   } else {
-    safeDelete(this, "require")";
+    safeDelete(this, "require");
   }
 
   if (this.__AC_OLD_MODULE) {
     this.module = this.__AC_OLD_MODULE;
     safeDelete(this, "__AC_OLD_MODULE");
   } else {
-    safeDelete(this, "module")";
+    safeDelete(this, "module");
   }
 }).call(this);
