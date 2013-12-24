20ff87bd9128489b15d4f88f30cf7ae366ee5534
Adrian Lanning <alanning@gmail.com>
2013-12-23 21:20:04 -0500

    add IE8 support
    delete called on the window object throws "object doesn't support this action" in IE8

--- revert-require-shim.js
+++ revert-require-shim.js
@@ -1,17 +1,28 @@
 // This file undoes all the trickery that we needed to get timezone working.
 (function(undefined) {
 
+  function safeDelete (target, field) {
+    try {
+      delete target[field]
+    }
+    catch (ex) {
+      // calling delete on a window field in IE8 throws an 
+      // "object doesn't support this action" error
+      target[field] = undefined
+    }
+  }
+  
   if (this.__AC_OLD_REQUIRE) {
     this.require = this.__AC_OLD_REQUIRE;
-    delete this.__AC_OLD_REQUIRE;
+    safeDelete(this, "__AC_OLD_REQUIRE");
   } else {
-    delete this.require;
+    safeDelete(this, "require")";
   }
 
   if (this.__AC_OLD_MODULE) {
     this.module = this.__AC_OLD_MODULE;
-    delete this.__AC_OLD_MODULE;
+    safeDelete(this, "__AC_OLD_MODULE");
   } else {
-    delete this.module;
+    safeDelete(this, "module")";
   }
 }).call(this);
